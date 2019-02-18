23c0e43af2248efbfd81ea6f63d9278a17c1f531
Reinier044 <reinier044@gmail.com>
2019-02-18 16:42:30 +0100

    Regular progress update

--- shearcenter.py
+++ shearcenter.py
@@ -1,3 +1,4 @@
+import numpy as np
 Ca      =   0.547       #[m]
 la      =   2.771       #[m]
 x1      =   0.153       #[m]
@@ -16,10 +17,35 @@ d3      =   1.642*10**-2 #[m]
 theta   =   26          #[deg]
 P       =   91700       #[N]
 q       =   4530        #[N/m]
+Iyy     =   261143990,8*10**-12     #[m^4]
+Izz     =   11991695,56*10**-12     #[m^4]
 
-x = la/2
-y = 0
-z = Ca/2
+radius = h/2                #[m]
+tribase =   Ca-radius       #[m], length of triangle base 
 
 
-print x,y,z
+#Produce mock locations for intermediate usage
+xmock = la/2
+ymock = 0
+zmock = Ca/2
+
+print xmock,ymock,zmock
+
+#END OF MOCK RESULTS
+
+Sy = 1
+
+S12 = radius                            #length of segment 12
+S23 = np.sqrt(radius**2+tribase**2)     #length of segment 23
+
+qb23 = -Sy/Izz*(radius/(2*S23))*s1**2           #basic shearflow calculation segment 23
+qb12 = -(Sy/Izz*(radius)*s2)-(Sy/Izz*(radius/(2*S23))*S23**2) #basic shearflow calculation segment 12
+
+
+
+
+
+
+
+
+
